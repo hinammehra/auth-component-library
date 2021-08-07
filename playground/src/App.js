30cbbcbf3e0cdaae052137ec653821d1f015e6fb
Prabhav Mehra <s3743761>
2021-08-07 12:59:47 +1000

    set-up component library with hello-world component

---
+++ playground/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HelloWorld } from 'auth-component-library';
+
+const App = ()  => {
+    return (
+        <HelloWorld />
+    );
+}
+
+export default App;
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HelloWorld() {
+    return <h1>Hello World 123</h1>
+}
+
+export default HelloWorld;
